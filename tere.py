1ddd4f6d434091c46c596cb6959156c263d998e7
Rando <rtoming@gmail.com>
2026-09-05 19:56:00 +0300

    Peale ülesande teksti lisamist...

--- tere.py
+++ tere.py
@@ -1,5 +1,25 @@
-print ("Tere!")
-a=2
-b=3
-c=a+b
-print (c, "see sama number")
+# Tähtaeg: pühapäev, 20. september 2026, 23.59 PM
+# Ülesanne koosneb kolmest osast. Kõik ülesande tegevused peavad olema esitatud ühes käivitatavas Pythoni skriptis:
+
+# Esimene osa:
+#  Koostada programm, mis väljastab ekraanile teksti: Hakkame programmeerima!
+
+# Teine osa:
+# 1. real luuakse muutuja nimega aasta ning antakse sellele väärtuseks 2026. (arvuna);
+# 2. real luuakse muutuja nimega koht ning antakse sellele väärtuseks "Tartu " (sõnena);
+# 3. real luuakse muutuja nimega lause_keskosa ning antakse sellele väärtuseks " on Eesti noortepealinn." (sõnena);
+# 4. real luuakse muutuja nimega lause, mille väärtuse saamiseks ühendatakse üheks sõnaks muutujad aasta, lause_keskosa ja koht (vajadusel tuleb kasutada funktsiooni, mis teisendab arvu sõneks);
+# 5. real väljastatakse muutuja lause väärtus ekraanile.
+
+# Kolmas osa:
+# kasutajalt küsitakse astme alus (täisarv) ja astendaja (täisarv);
+# arvutab ja väljastab ekraanile tulemuse.
+# Näiteks kui kasutaja sisestab vastustena 3 ja 2, siis väljastatakse 9.
+
+# Salvestage programm nimega: õpinguraamatunumber_x.py.
+# Õpinguraamatu number on Teile määratud 6 kohaline numbrite jada.
+# x - tähistab ülesande numbrit
+# Näide: 123456_1.py
+# Veenduge, et Teie programm töötab ja seejärel laadige koostatud programm siia üles.
+
+print ("Tere! Hakkame programmeerima!")
