2daf2cea2fa819355ccc8fe3756b6066c6fe9a42
Rando <rtoming@gmail.com>
2026-09-18 18:00:22 +0300

    Final

--- 110649-2.py
+++ 110649-2.py
@@ -3,19 +3,11 @@
 # Esimene osa:
 # Puhas vesi hakkab keema temperatuuril 100 oC. Samas ei tõuse vee temperatuur normaalrõhul ka üle 100 kraadi. 
 # Kogu saadud energia kulub vee aurustumisele.
-
 # Koostada programm, mis küsib vee temperatuuri täisarvudes ning väljastab vastuse:
 # 1. Vee temperatuur on (sisestatud väärtus) kraadi, vesi veel ei kee.
 # 2. Vee temperatuur on (sisestatud väärtus) kraadi, vesi keeb.
 # 3. Vee temperatuur ei saa olla üle 100 oC.
 # 4. Vesi on jääs.
-
-# Teine osa:
-# Teadupärast on kosmosejaamas ruum piiratud. Ent ometigi tehakse ka rahvusvahelisse kosmosejaama aeg-ajalt turismireise. Ütleme, et inimesed, keda lubatakse rahvusvahelisse kosmosejaama peavad olema kasvult maksimaalselt 190 cm (kaasa arvatud) pikad. Et eristada turiste töötajatest on töötajatel kaelakaart ja turistidel pilet.
-# Koostada programm, mis küsib reisija pikkust sentimeetrites. Seejärel küsib kas reisijal on pilet (jah, ei) seejärel küsib kas reisijal on kaelakaart (jah, ei).
-# Saanud reisijalt vastuse, et ta on alla 190 cm ja omab kas kaelakaarti või piletit, ilmub ekraanile kiri, et reisija pääseb kosmoselennule.
-# Kui aga reisija on pikem kui 190 cm või ei oma kaelakaarti ega piletit, ilmub kiri, et reisija ei pääse kosmoselennule.
-# Pange tähele, et vastus (jah või ei)võib olla kirjutatud ka suurte tähtedega.
 
 veetemperatuur = int(input("Sisesta vee temperatuur: "))
 if veetemperatuur < 0:
@@ -26,3 +18,25 @@ elif veetemperatuur == 100:
     print("Vesi keeb")    
 elif veetemperatuur > 100:
     print("Vesi muudab olekut, see aurustub!")
+
+
+# Teine osa:
+# Teadupärast on kosmosejaamas ruum piiratud. Ent ometigi tehakse ka rahvusvahelisse kosmosejaama aeg-ajalt turismireise. 
+# Ütleme, et inimesed, keda lubatakse rahvusvahelisse kosmosejaama peavad olema kasvult maksimaalselt 190cm (kaasa arvatud) pikad. 
+# Et eristada turiste töötajatest on töötajatel kaelakaart ja turistidel pilet.
+# Koostada programm, mis küsib reisija pikkust sentimeetrites. 
+# Seejärel küsib kas reisijal on pilet (jah, ei) 
+# seejärel küsib kas reisijal on kaelakaart (jah, ei).
+# Saanud reisijalt vastuse, et ta on alla 190 cm ja omab kas kaelakaarti või piletit, ilmub ekraanile kiri, 
+# et reisija pääseb kosmoselennule.
+# Kui aga reisija on pikem kui 190 cm või ei oma kaelakaarti ega piletit, ilmub kiri, et reisija ei pääse kosmoselennule.
+# Pange tähele, et vastus (jah või ei) võib olla kirjutatud ka suurte tähtedega.
+
+pikkus = int(input("Sisesta enda pikkus 'cm': "))
+pilet = str(input("Kas sul on pilet JAH/EI? "))
+kaart = str(input("Kas sul on kaelakaart JAH/EI? "))
+
+if pikkus <= 190 and (pilet == "JAH" or kaart == "JAH"):
+    print("Reisija pääseb lennule!")
+else:
+    print("Reisija ei pääse lennule")
