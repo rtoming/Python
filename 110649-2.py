0f7be3ddd83cd0c1f7b3bb8dcc7488b00f523648
Rando <rtoming@gmail.com>
2026-09-18 17:40:15 +0300

    2

--- 110649-2.py
+++ 110649-2.py
@@ -1,7 +1,9 @@
 # Ülesanne koosneb kahest osast. Kõik ülesande tegevused peavad olema esitatud ühes käivitatavas Pythoni skriptis:
 
 # Esimene osa:
-# Puhas vesi hakkab keema temperatuuril 100 oC. Samas ei tõuse vee temperatuur normaalrõhul ka üle 100 kraadi. Kogu saadud energia kulub vee aurustumisele.
+# Puhas vesi hakkab keema temperatuuril 100 oC. Samas ei tõuse vee temperatuur normaalrõhul ka üle 100 kraadi. 
+# Kogu saadud energia kulub vee aurustumisele.
+
 # Koostada programm, mis küsib vee temperatuuri täisarvudes ning väljastab vastuse:
 # 1. Vee temperatuur on (sisestatud väärtus) kraadi, vesi veel ei kee.
 # 2. Vee temperatuur on (sisestatud väärtus) kraadi, vesi keeb.
@@ -14,3 +16,13 @@
 # Saanud reisijalt vastuse, et ta on alla 190 cm ja omab kas kaelakaarti või piletit, ilmub ekraanile kiri, et reisija pääseb kosmoselennule.
 # Kui aga reisija on pikem kui 190 cm või ei oma kaelakaarti ega piletit, ilmub kiri, et reisija ei pääse kosmoselennule.
 # Pange tähele, et vastus (jah või ei)võib olla kirjutatud ka suurte tähtedega.
+
+veetemperatuur = int(input("Sisesta vee temperatuur: "))
+if veetemperatuur < 0:
+    print("Vesi on jääs!")
+elif veetemperatuur < 100:
+    print("Vesi veel ei kee")
+elif veetemperatuur == 100:
+    print("Vesi keeb")    
+elif veetemperatuur > 100:
+    print("Vesi muudab olekut, see aurustub!")
